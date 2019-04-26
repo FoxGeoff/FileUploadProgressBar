7ce3a43df2ace55d2d67db0b95205034f145898a
FoxGeoff <fox.geoffrey@gmail.com>
2019-04-25 18:54:07 -0700

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FileUploadProgressBar';
+
+  fileData: File = null;
+  constructor(private http: HttpClient) { }
+
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http.post('url/to/your/api', formData, {
+      reportProgress: true,
+      observe: 'events'
+    })
+      .subscribe(events => {
+        if (events.type == HttpEventType.UploadProgress) {
+          console.log('Upload progress: ', Math.round(events.loaded / events.total * 100) + '%');
+        } else if (events.type === HttpEventType.Response) {
+          console.log(events);
+        }
+      })
+  }
 }
